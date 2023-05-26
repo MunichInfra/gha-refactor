3337a7c35d9b228bbe2b361bfb183332bccb7f27
FaridS <abc@example.com>
2023-05-26 16:30:15 +0200

    Adding terraform code

---
+++ terraform/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 1.0"
+
+  backend "s3" {}
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.8"
+    }
+  }
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "farid-tf-test-2023-0526"
+}
